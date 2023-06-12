a1363d6050e558b87c07551fca097fe4b4d73870
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-06-12 17:00:37 +0900

    fix: import setting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
-import mailController from "./mailController";
+const mailController = require("./mailController");
 
 dotenv.config();
 
