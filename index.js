4e0b9abdf427b30b2c6e7e5bf3bfa9080adc7b6d
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-06-14 16:43:52 +0900

    fix: set netlify deploy option

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const mailController = require("./mailController");
-
-dotenv.config();
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(cors());
-app.use("/api/contact", mailController.sendEmail);
-
-app.get("/", (req, res) => res.send("Tyrese Personal WebSite Backend!"));
+const app = require("./server");
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mailController = require("./mailController");
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use("/api/contact", mailController.sendEmail);
+
+app.get("/", (req, res) => res.send("Tyrese Personal WebSite Backend!"));
+
+module.exports = app;
