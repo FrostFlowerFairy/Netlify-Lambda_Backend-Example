fca34777bb6a6d63b4566368c40c6d0d251fa4a6
Wizardly3915 <wizardlydeveloper@gmail.com>
2023-06-14 17:37:50 +0900

    fix: set netlify deploy option

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const serverless = require("serverless-http");
 const mailController = require("./mailController");
 
 dotenv.config();
@@ -13,9 +14,9 @@ app.use(bodyParser.json());
 
 app.use(cors());
 app.use("/api/contact", mailController.sendEmail);
+app.use("/.netlify/functions/api/contact", mailController.sendEmail); // path must route to lambda
 
 app.get("/", (req, res) => res.send("Tyrese Personal WebSite Backend!"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+module.exports = app;
+module.exports.handler = serverless(app);
